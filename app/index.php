43959b6a9c35672aefc273c63c226c20a1fb5da8
Yuto Takano <moa17stock@gmail.com>
2019-06-03 17:21:00 +0900

    Make sure index.php can handle LINE requests (even though it never will)

--- app/index.php
+++ app/index.php
@@ -10,4 +10,26 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$post_data = file_get_contents('php://input');
+
+ob_start();
+$json = json_decode($post_data, true);
+if (!$json) {
+  echo 'Invalid JSON payload.' . date('Y-m-d H:i:s');
+}
+
+$log = ob_get_clean();
+
+// Don't write in log if there was no output
+if (!empty(trim($log))) {
+  $log .= PHP_EOL . "----------------------------" . PHP_EOL . PHP_EOL;
+  $logFile = "logs/log_" . date("Y.m.d") . ".txt";
+  file_put_contents($logFile, $log, FILE_APPEND);
+  chmod($logFile, 0777);
+}
+
+
 http_response_code(200);
